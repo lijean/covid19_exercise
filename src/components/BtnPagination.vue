9d014b897e38f2903853bd72b78b441f4e4363e7
lijean <ycess87177@gmail.com>
2022-04-15 16:48:18 +0800

    新增縣市確診表單

---
+++ src/components/BtnPagination.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="page text-center">
+        <ul class="pagination flex-wrap justify-content-center justify-content-xl-end" v-show="totalPages>1">
+            <li class="page-item" :class="{'disabled' : currentPage <= 1}">
+            <button class="page-link" @click="$emit('change',currentPage-1)">Prev</button>
+            </li>
+            <li class="page-item" v-for="page of totalPages" :key="page"
+                :class="{'active' : page === currentPage}"
+                v-show="page<=currentPage+2 && page>=currentPage-2">
+                <button class="page-link" @click="$emit('change',page)">{{ page }}</button>
+            </li>
+            <li class="page-item" :class="{'disabled' : currentPage >= totalPages}">
+            <button class="page-link" @click="$emit('change',currentPage+1)">Next</button>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'ButtonPagination',
+  props: {
+    totalPages: Number,
+    currentPage: Number
+  }
+}
+</script>
--- src/views/CityView.vue
+++ src/views/CityView.vue
@@ -6,6 +6,53 @@
                     :itemList="cityLocalList"
                     :itemIndex="cityCurrentIndex"
                     @filter="changeIndexHandler"/>
+      <div class="row align-items-start py-5">
+        <div class="col-xl-7 col-12">
+
+          <div class="d-flex flex-wrap justify-content-end justify-content-xl-start mb-3">
+            <div class="d-flex align-items-center">
+              <span>顯示筆數</span>
+              <select class="form-select form-select-sm w-auto ms-2" v-model="perPageIndex">
+                <option v-for="page,index of perPageList"
+                        :key="page"
+                        :value="index">
+                  {{ page }}
+                </option>
+              </select>
+            </div>
+          </div>
+
+          <div class="table-responsive text-center">
+            <table class="table table-striped table-sm">
+              <thead>
+                <tr>
+                  <th scope="col">NO.</th>
+                  <th scope="col">日期</th>
+                  <th scope="col">縣市</th>
+                  <th scope="col">鄉鎮</th>
+                  <th scope="col">性別</th>
+                  <th scope="col">年齡層</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="item,index of pageList" :key="item.id">
+                  <td>{{ (this.currentPage - 1) * this.perPage + index + 1 }}</td>
+                  <td>{{ item.date }}</td>
+                  <td>{{ item.city }}</td>
+                  <td>{{ item.town }}</td>
+                  <td>{{ item.gender }}</td>
+                  <td>{{ item.age }}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+
+          <ButtonPagination class="mt-2"
+                            :totalPages="totalPages"
+                            :currentPage="currentPage"
+                            @change="changeCurrentPage"/>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -13,12 +60,14 @@
 <script>
 import PageTitle from '@/components/PageTitle.vue'
 import ButtonFilter from '@/components/BtnFilter.vue'
+import ButtonPagination from '@/components/BtnPagination.vue'
 
 export default {
   name: 'CityView',
   components: {
     PageTitle,
-    ButtonFilter
+    ButtonFilter,
+    ButtonPagination
   },
   props: {
     dataList: Array,
@@ -51,19 +100,9 @@ export default {
       })
 
       return obj
-    // },
-    // localList () {
-    //   const cityId = this.city.currentIndex
-    //   const prevLength = this.city.prevList.length
-    //   if (cityId >= prevLength) {
-    //     const getCity = this.cityList.sort[cityId - (prevLength - 1)]
-    //     return this.dataList.filter(({ city }) => city === getCity)
-    //   } else {
-    //     return this.dataList.filter(({ overseas }) => !overseas)
-    //   }
     },
-    cityLocalList () {
-      const cityPrevList = [
+    cityPrevList () {
+      return [
         {
           title: '全部縣市',
           num: this.dataList
@@ -71,6 +110,8 @@ export default {
             .filter(({ overseas }) => !overseas).length
         }
       ]
+    },
+    cityLocalList () {
       const cityAfterList = this.cityList.sort.filter((item, index) => index !== 0)
         .map(item => {
           return {
@@ -79,12 +120,36 @@ export default {
           }
         })
 
-      return cityPrevList.concat(cityAfterList)
+      return this.cityPrevList.concat(cityAfterList)
+    },
+    localList () {
+      const prevLength = this.cityPrevList.length
+      if (this.cityCurrentIndex >= prevLength) {
+        const getCity = this.cityList.sort[this.cityCurrentIndex - (prevLength - 1)]
+        return this.dataList.filter(({ city }) => city === getCity)
+      } else {
+        return this.dataList.filter(({ overseas }) => !overseas)
+      }
+    },
+    perPage () {
+      return this.perPageList[this.perPageIndex]
+    },
+    pageList () {
+      const start = (this.currentPage - 1) * this.perPage
+      const end = start + this.perPage
+      return this.localList.slice(start, end)
+    },
+    totalPages () {
+      return Math.ceil(this.localList.length / this.perPage)
     }
   },
   methods: {
     changeIndexHandler (value) {
       this.cityCurrentIndex = value
+      this.changeCurrentPage(1)
+    },
+    changeCurrentPage (page) {
+      this.currentPage = (page + this.totalPages + 1) % (this.totalPages + 1)
     }
   }
 }
